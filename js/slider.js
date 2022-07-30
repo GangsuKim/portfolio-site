275f35f7e9c3313b835dfc719f68b75668022ae7
GangsuKim <gangsu1813@gmail.com>
2022-07-30 18:20:24 +0900

    Add 2nd project

--- js/slider.js
+++ js/slider.js
@@ -1,23 +1,26 @@
 window.onload = () => {
     const slider1 = ['SIGN/Sign_1.png','SIGN/Sign_2.png'];
+    const slider2 = ['BeanPlace/BeanPlace_1.png','BeanPlace/BeanPlace_2.png','BeanPlace/BeanPlace_3.png','BeanPlace/BeanPlace_4.png','BeanPlace/BeanPlace_5.png'];
     const slidercnt = [];
     
-    const sliders = [slider1];
+    const sliders = [slider1,slider2];
     const path = './images/';
 
     const imageSlider = document.getElementsByClassName("imageSlider");
 
-    const cnt = 0;
+    var cnt = 0;
     for (let slider of imageSlider) {
+        console.log(slider)
+        slider.number = cnt;
         slidercnt.push(0); // set image status
         slider.querySelector('.btn.leftBtn').addEventListener('click', (e) => {
-            slider.sliderCnt = --slidercnt[cnt];
-            sliderLeft(slider,sliders[cnt])
+            slider.sliderCnt = ++slidercnt[slider.number];
+            sliderLeft(slider,sliders[slider.number])
         });
 
         slider.querySelector('.btn.rightBtn').addEventListener('click', (e) => {
-            slider.sliderCnt = ++slidercnt[cnt];
-            sliderRight(slider,sliders[cnt])
+            slider.sliderCnt = --slidercnt[slider.number];
+            sliderRight(slider,sliders[slider.number])
         });
 
 
@@ -32,18 +35,19 @@ window.onload = () => {
                 slider.querySelector('.bottomLists').innerHTML += circle.outerHTML;
             }
         }
+        cnt++;
     }
 
     function sliderLeft(slider,sliderList) {
         slider.querySelector('img').src = path + sliderList[Math.abs(slider.sliderCnt%sliderList.length)];
         slider.querySelector('.bottomLists').querySelector('#btn_' + Math.abs(slider.sliderCnt%sliderList.length)).setAttribute('class','listCircle now');
-        slider.querySelector('.bottomLists').querySelector('#btn_' + Math.abs((slider.sliderCnt + 1)%sliderList.length)).setAttribute('class','listCircle');
+        slider.querySelector('.bottomLists').querySelector('#btn_' + Math.abs((slider.sliderCnt - 1)%sliderList.length)).setAttribute('class','listCircle');
     }
 
     function sliderRight (slider,sliderList) {
         slider.querySelector('img').src = path + sliderList[Math.abs(slider.sliderCnt%sliderList.length)];
         slider.querySelector('.bottomLists').querySelector('#btn_' + Math.abs(slider.sliderCnt%sliderList.length)).setAttribute('class','listCircle now');
-        slider.querySelector('.bottomLists').querySelector('#btn_' + Math.abs((slider.sliderCnt - 1)%sliderList.length)).setAttribute('class','listCircle');
+        slider.querySelector('.bottomLists').querySelector('#btn_' + Math.abs((slider.sliderCnt + 1)%sliderList.length)).setAttribute('class','listCircle');
 
     }
 }
