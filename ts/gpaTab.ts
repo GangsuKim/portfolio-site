c46db314a7b161f966495d565d50e8836974761d
GangsuKim <gangsu1813@gmail.com>
2022-08-15 13:03:04 +0900

    Convert to Typescript

---
+++ ts/gpaTab.ts
@@ -0,0 +1,40 @@
+let active: string = '1-1'; // 현재 활성화 되어 있는 Board의 Season
+let lang: string = 'kr'; // 언어 사전 설정
+
+const gpa_tab: NodeListOf<HTMLElement> = document.querySelectorAll('.gpa-tab'); // Tab elements
+
+for (var i = 0; i < gpa_tab.length; i++) { // Tab의 개수만큼 실행 (8회)
+    gpa_tab[i].addEventListener('click', (e) => { // 클릭시 Event 발생
+        if (e.target instanceof HTMLElement) {
+            var season = e.target.innerHTML;
+            if (e.target.localName == 'li') {
+                season = e.target.id;
+            }
+            changeBoardTo(season); // Board를 변경하는 함수 실행 (param : season)
+        }
+    });
+}
+
+function changeBoardTo(season :string) { // Board를 변경하는 함수
+    $.getJSON('../json/' + season + '.json', function (data) { // Season => (ex. 1-1, 1-2) 에 해당하는 Json 파일 로드
+        $('.boardTitle').text(data['season_' + lang]); // 학기 표시 (ex. 1학년 1학기)
+        $('.gpaCount').text(data['course-count']); // 수강 학점 표시 (ex. 16)
+
+        let html :string = ''; // Table HTML stack
+        for (var i = 0; i < data['course'].length; i++) {
+            const course = data.course[i];
+            let styleSet :string = '';
+            if (course["name_" + lang].length > 20) {
+                styleSet = 'style="font-size: 80%;"';
+            } else if (course["name_" + lang].length > 30) {
+                styleSet = 'style="font-size: 60%;"';
+            }
+            html += '<tr><td>' + course["type_" + lang] + '</td><td ' + styleSet + '>' + course["name_" + lang] + '</td><td>' + course["credit"] + '</td><td>' + course["grade"] + '</td></tr>';
+        }
+        $('.board > table > tbody').html(html);
+
+        $('#' + active).removeClass('selected');
+        $('#' + season).addClass('selected');
+        active = season
+    })
+}
